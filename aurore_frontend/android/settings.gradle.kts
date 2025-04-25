245c2a9c6f9c9f9f8e8a10f43ed7b29fa4e78d6c
Kairos2025 <mbuladavid04@gmail.com>
2025-04-25 14:57:56 +0300

    Update settings.gradle.kts

--- aurore_frontend/android/settings.gradle.kts
+++ aurore_frontend/android/settings.gradle.kts
@@ -8,11 +8,11 @@ pluginManagement {
     // Safe Flutter SDK path resolution
     val localProperties = file("../local.properties")
     if (localProperties.exists()) {
-        val properties = java.util.Properties()
-        localProperties.inputStream().use { properties.load(it) }
-        properties.getProperty("flutter.sdk")?.let { flutterSdkPath ->
-            if (file("$flutterSdkPath/packages/flutter_tools/gradle").exists()) {
-                includeBuild(flutterSdkPath + "/packages/flutter_tools/gradle")
+        properties.load(localProperties.inputStream())
+        properties.getProperty("flutter.sdk")?.let { sdkPath ->
+            val flutterToolsPath = file("$sdkPath/packages/flutter_tools/gradle")
+            if (flutterToolsPath.exists()) {
+                includeBuild(flutterToolsPath)
             }
         }
     }
@@ -20,7 +20,7 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
