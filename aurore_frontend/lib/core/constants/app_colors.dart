44a085d43c7e73f0e8b9c10aa647873ac72fb052
Kairos2025 <mbuladavid04@gmail.com>
2025-05-10 10:37:07 +0300

    Update app_colors.dart

--- aurore_frontend/lib/core/constants/app_colors.dart
+++ aurore_frontend/lib/core/constants/app_colors.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Colors.blue;
-  static const Color secondary = Colors.blueAccent;
+  static const Color primary = Color(0xFF003087); // Dark blue for app bar background
+  static const Color secondary = Color(0xFFF5C400); // Yellow for accents
   static const Color background = Colors.white;
-  static const Color darkGrey = Colors.grey;
+  static const Color darkGrey = Colors.grey; // Darker grey for emphasis
   static const Color white = Colors.white;
-  static const Color neutral = Colors.grey;
+  static const Color neutral = Colors.grey; // Lighter grey for neutral elements
   static const Color error = Colors.red;
   static const Color textSecondary = Colors.black54;
+  static const Color iconPrimary = Colors.white; // For consistent icon tinting
 }
