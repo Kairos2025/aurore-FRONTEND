4f859b79bfd83608859a10c27fd3ee91ee30a9bc
Kairos2025 <mbuladavid04@gmail.com>
2025-05-10 16:57:14 +0300

    Update aurore_button.dart

--- aurore_frontend/lib/widgets/aurore_button.dart
+++ aurore_frontend/lib/widgets/aurore_button.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:aurore_school/core/constants/app_colors.dart';
+import 'package:aurore_school/core/constants/app_text_styles.dart';
 
 class AuroreButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
   final IconData? icon;
+  final Color? iconColor;
 
   const AuroreButton({
     super.key,
     required this.text,
     required this.onPressed,
     this.icon,
+    this.iconColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
       onPressed: onPressed,
-      icon: icon != null ? Icon(icon) : const SizedBox.shrink(),
-      label: Text(text),
+      icon: icon != null
+          ? Icon(
+              icon,
+              color: iconColor ?? AppColors.iconPrimary,
+            )
+          : const SizedBox.shrink(),
+      label: Text(
+        text,
+        style: AppTextStyles.button,
+      ),
       style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        foregroundColor: AppColors.iconPrimary,
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8),
