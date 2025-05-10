63fc302d4b9f1a2046effb1bdedd7181676dd507
Kairos2025 <mbuladavid04@gmail.com>
2025-05-10 16:52:40 +0300

    Update app_text_styles.dart

--- aurore_frontend/lib/utils/app_text_styles.dart
+++ aurore_frontend/lib/utils/app_text_styles.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:aurore_school/core/constants/app_colors.dart';
 
 class AppTextStyles {
-  static const TextStyle title = TextStyle(
+  static const TextStyle appBarTitle = TextStyle(
+    fontFamily: 'Roboto',
     fontSize: 20,
     fontWeight: FontWeight.bold,
+    color: AppColors.iconPrimary,
+  );
+
+  static const TextStyle header = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+
+  static const TextStyle subheader = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.textSecondary,
+  );
+
+  static const TextStyle title = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
   );
 
   static const TextStyle body = TextStyle(
+    fontFamily: 'Roboto',
     fontSize: 16,
+    color: AppColors.textSecondary,
   );
 
   static const TextStyle bodyBold = TextStyle(
+    fontFamily: 'Roboto',
     fontSize: 16,
     fontWeight: FontWeight.bold,
+    color: AppColors.textSecondary,
+  );
+
+  static const TextStyle button = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.iconPrimary,
+  );
+
+  static const TextStyle label = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    color: AppColors.textSecondary,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    color: AppColors.neutral,
+  );
+
+  static const TextStyle error = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    color: AppColors.error,
   );
 }
