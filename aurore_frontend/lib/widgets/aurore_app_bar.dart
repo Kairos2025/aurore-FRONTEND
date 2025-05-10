8e87f3299165d9dba429e2904d9db15528ea5bbc
Kairos2025 <mbuladavid04@gmail.com>
2025-05-10 16:54:52 +0300

    Update aurore_app_bar.dart

--- aurore_frontend/lib/widgets/aurore_app_bar.dart
+++ aurore_frontend/lib/widgets/aurore_app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:aurore_school/core/constants/app_colors.dart';
+import 'package:aurore_school/core/constants/app_text_styles.dart';
 
 class AuroreAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
@@ -9,9 +11,20 @@ class AuroreAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text(title),
+      leading: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset(
+          'assets/images/aurore_logo.png',
+          width: 32,
+          height: 32,
+        ),
+      ),
+      title: Text(
+        title,
+        style: AppTextStyles.appBarTitle,
+      ),
       actions: actions,
-      backgroundColor: Theme.of(context).primaryColor,
+      backgroundColor: AppColors.primary,
     );
   }
 
