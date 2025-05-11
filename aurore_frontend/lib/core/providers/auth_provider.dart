742384ca586e70bb3ebedab2922dab8ed371ad8c
Kairos2025 <mbuladavid04@gmail.com>
2025-05-12 01:08:47 +0300

    Update auth_provider.dart

--- aurore_frontend/lib/core/providers/auth_provider.dart
+++ aurore_frontend/lib/core/providers/auth_provider.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:aurore_school/utils/secure_storage.dart';
+import '../../utils/secure_storage.dart';
 
 class AuthProvider extends ChangeNotifier {
   final Dio _dio;
