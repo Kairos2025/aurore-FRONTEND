a793557bf7267a625063c6d0a7642c4089d7a9c5
Kairos2025 <mbuladavid04@gmail.com>
2025-05-12 01:27:40 +0300

    Update aurore_button.dart

--- aurore_frontend/lib/widgets/aurore_button.dart
+++ aurore_frontend/lib/widgets/aurore_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:aurore_school/core/constants/app_colors.dart';
-import 'package:aurore_school/core/constants/app_text_styles.dart';
+import '../core/constants/app_colors.dart';
+import '../core/constants/app_text_styles.dart';
 
 class AuroreButton extends StatelessWidget {
   final String text;
