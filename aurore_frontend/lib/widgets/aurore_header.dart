2f09de36ac43b6c21074f3e39d7e5b1025835fb9
Kairos2025 <mbuladavid04@gmail.com>
2025-05-12 01:28:24 +0300

    Update aurore_header.dart

--- aurore_frontend/lib/widgets/aurore_header.dart
+++ aurore_frontend/lib/widgets/aurore_header.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:aurore_school/core/constants/app_text_styles.dart';
+import '../core/constants/app_text_styles.dart';
 
 class AuroreHeader extends StatelessWidget {
   final String title;
